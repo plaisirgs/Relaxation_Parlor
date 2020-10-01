18aae333c3d5feb995aebf34b3a1348c98873446
plaisirgs <63075823+plaisirgs@users.noreply.github.com>
2020-09-30 21:07:52 -0400

    Added More Massage Therapists

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,24 +8,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 MassageTherapist.create(first_name: 'Lindsay', years_of_experience: 5, specialty: 'Swedish Massage')
-MassageTherapist.create(first_name: 'Logan’, years_of_experience: 5, specialty: 'Swedish Massage')
+MassageTherapist.create(first_name: 'Logan', years_of_experience: 5, specialty: 'Swedish Massage')
 MassageTherapist.create(first_name:  'Alfred', years_of_experience: 7, specialty: 'Shiatsu Massage')
-MassageTherapist.create(first_name:  'Almaretta’, years_of_experience: 7, specialty: 'Shiatsu Massage')
+MassageTherapist.create(first_name:  'Almaretta', years_of_experience: 7, specialty: 'Shiatsu Massage')
 MassageTherapist.create(first_name:  'Hank', years_of_experience: 2, specialty: 'Hot Stone Massage')
-MassageTherapist.create(first_name:  'Harriet, years_of_experience: 5, specialty: 'Hot Stone Massage')
+MassageTherapist.create(first_name:  'Harriet', years_of_experience: 5, specialty: 'Hot Stone Massage')
 MassageTherapist.create(first_name:  'Melissa', years_of_experience: 9, specialty: 'Aromatherapy Massage')
-MassageTherapist.create(first_name:  'Monte’, years_of_experience: 4, specialty: 'Aromatherapy Massage')
+MassageTherapist.create(first_name:  'Monte', years_of_experience: 4, specialty: 'Aromatherapy Massage')
 MassageTherapist.create(first_name:  'Sergio', years_of_experience: 4, specialty: 'Deep Tissue Massage')
-MassageTherapist.create(first_name:  'Samantha’, years_of_experience: 7, specialty: 'Deep Tissue Massage')
+MassageTherapist.create(first_name:  'Samantha', years_of_experience: 7, specialty: 'Deep Tissue Massage')
 MassageTherapist.create(first_name:  'Alice', years_of_experience: 6, specialty: 'Reflexology Massage')
-MassageTherapist.create(first_name:  'Alexander’, years_of_experience: 3, specialty: 'Reflexology Massage')
+MassageTherapist.create(first_name:  'Alexander', years_of_experience: 3, specialty: 'Reflexology Massage')
 MassageTherapist.create(first_name:  'Camilla', years_of_experience: 8, specialty: 'Prenatal Massage')
-MassageTherapist.create(first_name:  ‘Charlie’, years_of_experience: 6, specialty: 'Prenatal Massage')
+MassageTherapist.create(first_name:  'Charlie', years_of_experience: 6, specialty: 'Prenatal Massage')
 MassageTherapist.create(first_name:  'Nate', years_of_experience: 3, specialty: 'Lymphatic Massage')
-MassageTherapist.create(first_name:  'Natalia,’ years_of_experience: 4, specialty: 'Lymphatic Massage')
-
-
-
-
-
-
+MassageTherapist.create(first_name:  'Natalia', years_of_experience: 4, specialty: 'Lymphatic Massage')
