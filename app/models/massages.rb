7696fc60a825c2ada1a6236d6f2240ec3f583bc4
plaisirgs <63075823+plaisirgs@users.noreply.github.com>
2020-10-08 08:46:58 -0400

    Edited the Massages class

--- app/models/massages.rb
+++ app/models/massages.rb
@@ -1,19 +1,14 @@
 # frozen_string_literal: true
 
-class Massages < ActiveRecord::Base
+class Massages
   include Ruby::Enum
 
   define :LYMPHATIC, 'Lymphatic Massage $60/hr'
-  define :GREEN, 'green'
+  define :PRENATAL, 'Prenatal Massage $40/hr'
+  define :REFLEXOLOGY, 'Reflexology Massage $65/hr'
+  define :DEEP_TISSUE, 'Deep Tissue Massage $70/hr'
+  define :AROMATHERAPY, 'Aromatherapy Massage $50/hr'
+  define :HOT_STONE, 'Hot Stone Massage $80/hr'
+  define :SHIATSU, 'Shiatsu Massage $60/hr'
+  define :SWEDISH, 'Swedish Massage $75/hr'
 end
-
-
-
-# Massage.create(type_of_massage: 'Lymphatic Massage ', time_length: '60 mins', price: '$60')
-# Massage.create(type_of_massage: 'Prenatal Massage ', time_length: '60 mins', price: '$40')
-# Massage.create(type_of_massage: 'Reflexology Massage ', time_length: '60 mins', price: '$65')
-# Massage.create(type_of_massage: 'Deep Tissue Massage ', time_length: '60 mins', price: '$70')
-# Massage.create(type_of_massage: 'Aromatherapy Massage ', time_length: '60 mins', price: '$50')
-# Massage.create(type_of_massage: 'Hot Stone Massage ', time_length: '60 mins', price: '$80')
-# Massage.create(type_of_massage: 'Shiatsu Massage ', time_length: '60 mins', price: '$60')
-# Massage.create(type_of_massage: 'Swedish Massage ', time_length: '60 mins', price: '$75')
---
+++ db/migrate/20201008000220_add_massages_table.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class AddMassagesTable < ActiveRecord::Migration[6.0]
+  def change
+    create_table :massages do |t|
+      t.string :type_of_massage
+      t.string :time_length
+      t.string :price
+    end
+   end
+end
